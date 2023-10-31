e2adfe7b416d355987ae9392492194e5e23124c0
Jerico <jericodcosta95375788@gmail.com>
2023-10-31 16:16:40 +0530

    fc

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -1,17 +1,34 @@
-import React,{useState} from 'react';
-
+import React, { useState, useEffect } from "react";
+import markdownIt from "markdown-it";
+// import parse from "../parser";
 const Markdown = () => {
-    const[input, setInput] = useState("");
-    return (
-        <div className='Markdown'>
-                <div className='preview'>
-                    <h1 className='loading'>{input}</h1>
-                </div>
-                <div className='text-area'>
-                    <textarea onInput={(e)=>(setInput(e.target.value))} className='textarea'  rows={30} cols={60}></textarea>
-                </div>
-        </div>
-    );
-}
+  const [input, setInput] = useState("");
+  const [ans, setAns] = useState("");
+  useEffect(() => {
+    let md = new markdownIt();
+
+    let renderdHTML = md.render(input);
+    setAns(renderdHTML);
+  }, [input]);
+
+  return (
+    <div className="Markdown">
+      <div className="preview">
+        <div
+          dangerouslySetInnerHTML={{ __html: ans }}
+          className="loading"
+        ></div>
+      </div>
+      <div className="text-area">
+        <textarea
+          onInput={(e) => setInput(e.target.value)}
+          className="textarea"
+          rows={30}
+          cols={60}
+        ></textarea>
+      </div>
+    </div>
+  );
+};
 
 export default Markdown;
